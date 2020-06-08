640c7da785ea1cf38ff8e9974064fb8a3d62c711
Kevin Liang <kevinjiahongliang@gmail.com>
2020-06-08 04:51:12 -0700

    100 day moving average

--- finance.py
+++ finance.py
@@ -15,16 +15,19 @@ df = web.DataReader('TSLA', 'yahoo', start, end)
 df.reset_index(inplace=True)
 df.set_index("Date", inplace=True)
 
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+df.dropna(inplace=True)
 print(df.head())
 
-df['Adj Close'].plot()
+ax1 = plt.subplot2grid((6, 1), (0,0), rowspan = 5, colspan=1)
+ax2 = plt.subplot2grid((6, 1), (5,0), rowspan = 1, colspan=1, sharex=ax1)
+
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+
 plt.show()
-
-
-
-
-
-
 
 
 
